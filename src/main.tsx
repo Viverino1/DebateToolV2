ba6eb2c0cac2488c4536df70ca5245430158092a
Verge Robotics <VergeRoboticsFTC@gmail.com>
2023-07-13 21:39:05 -0500

    more CreateEvidencePage

--- src/main.tsx
+++ src/main.tsx
@@ -5,7 +5,7 @@ import './index.css'
 import { Provider } from 'react-redux'
 import store from './utils/redux/store.ts'
 import { BrowserRouter } from 'react-router-dom'
-import { QueryClient, QueryClientProvider } from 'react-query'
+import { QueryCache, QueryClient, QueryClientProvider } from 'react-query'
 import { getSchools, getTopics } from './utils/firebase/firestore/firestore.ts'
 import { createWebStoragePersistor } from "react-query/createWebStoragePersistor-experimental"
 import { persistQueryClient } from "react-query/persistQueryClient-experimental"
@@ -19,6 +19,15 @@ const queryClient = new QueryClient({
       staleTime: Infinity
     }
   }
+});
+
+const queryCache = new QueryCache({
+  onError: (error) => {
+    console.log(error);
+  },
+  onSuccess: (data) => {
+    console.log(data);
+  },
 });
 
 queryClient.prefetchQuery({
@@ -55,4 +64,4 @@ ReactDOM.createRoot(document.getElementById('root')!).render(
   </React.StrictMode>,
 )
 
-export{ queryClient }
+export{ queryClient, queryCache }
--- src/pages/cards/create/evidence/CreateEvidencePage.tsx
+++ src/pages/cards/create/evidence/CreateEvidencePage.tsx
@@ -1,11 +1,55 @@
+import { useState } from "react";
 import EvidenceCard from "../../../../components/cards/EvidenceCard";
 import { dummyEvidenceCard } from "../../../../utils/helpers";
+import { getCurrentUser } from "../../../../utils/firebase/firestore/firestore";
+import { useQuery } from "react-query";
+import { queryCache, queryClient } from "../../../../main";
 
 export default function CreateEvidencePage(){
+  const user = queryClient.getQueryData('currentUser') as User;
+
+  const [card, setCard] = useState<Evidence>({
+    type: "evidence",
+    cardID: "",
+    ownerUID: user.uid,
+    teamID: user.teamID,
+    school: user.school,
+    isPublic: false,
+    createTime: 0,
+    lastEditTime: 0,
+    title: "",
+    sourceName: "",
+    sourceLink: "",
+    data: "",
+    warrant: "",
+    contention: null,
+    subpoint: null,
+  });
+
   return(
     <div className="w-full h-full flex">
-      <div className="bg-primary w-full sm:w-2/3 h-full"></div>
-      <div className="bg-secondary w-1/3 h-full"></div>
+      <div className="w-full md:w-1/2 lg:w-2/3 h-full p-4 flex flex-col space-y-4">
+        <input type="text" 
+        className="input text-text-light" 
+        placeholder="Title"
+        onChange={e => {
+          setCard(old => ({...old, title: e.target.value}))
+        }}
+        />
+
+        <input 
+        type="text" 
+        className="input !text-lg" 
+        placeholder="Source Name"
+        onChange={e => {
+          setCard(old => ({...old, sourceName: e.target.value}))
+        }}
+        />
+        
+      </div>
+      <div className="hidden md:flex items-center md:w-1/2 lg:w-1/3 h-full p-4">
+        <div className="h-1/2 w-full"><EvidenceCard card={card}/></div>
+      </div>
     </div>
   )
 }

fe55d105eca99b85b340b912771ab04cc0f0d832
Vivek Maddineni <vivekmaddineni@gmail.com>
2024-12-15 09:33:42 -0600

    Fixed Bugs

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -1,5 +1,5 @@
-import { ReactElement } from "react"
-import { CardHeading, FileEarmarkText } from "react-bootstrap-icons"
+import { ReactElement } from "react";
+import { CardHeading } from "react-bootstrap-icons";
 import { useLocation, useNavigate } from "react-router-dom";
 import { useAppDispatch, useAppSelector } from "../utils/redux/hooks";
 import { setSide, setTopic } from "../utils/redux/reducers/appSlice";
@@ -7,30 +7,30 @@ import { getTopics } from "../utils/firebase/firestore/firestore";
 import { useQuery, useQueryClient } from "react-query";
 import Divider from "./Divider";
 
-export default function Sidebar(){
+export default function Sidebar() {
   const iconSize = 30;
-  return(
+  return (
     <div className="fixed top-0 left-0 bottom-0 z-50 w-22 h-full p-4 bg-background text-text-light flex flex-col justify-between">
       <div className="flex flex-col space-y-4">
-        <Icon 
-        icon={<img src="/DebateToolLogo.svg" className="w-12"/>}
-        text="Debate Tool"
-        link="/home"
+        <Icon
+          icon={<img src="/DebateToolLogo.svg" className="w-12" />}
+          text="Debate Tool"
+          link="/home"
         />
 
-        <Divider/>
+        <Divider />
 
-        <Icon 
-        icon={<CardHeading size={iconSize}/>}
-        text="Cards"
-        link="cards"
+        <Icon
+          icon={<CardHeading size={iconSize} />}
+          text="Cards"
+          link="cards"
         />
 
-        <Icon 
+        {/* <Icon 
         icon={<FileEarmarkText size={iconSize}/>}
         text="Case"
         link="case"
-        />
+        /> */}
 
         {/* <Icon 
         icon={<BootstrapReboot size={iconSize}/>}
@@ -38,99 +38,117 @@ export default function Sidebar(){
         link="rounds"
         /> */}
 
-        <Divider/>
+        <Divider />
 
-        <SideSelector/>
+        <SideSelector />
 
-        <TopicSelector/>
+        <TopicSelector />
       </div>
       <div className="flex flex-col space-y-4">
-        <Divider/>
+        <Divider />
 
-        <Icon 
-        icon={<img src="https://lh3.googleusercontent.com/ogw/AGvuzYb8Az9mueXWBCrZlm6FogE9a6D-WAWivPYEKrkB7g=s32-c-mo" className="w-10 rounded-full"/>}
-        text="Vivek Maddineni"
-        link="settings"
+        <Icon
+          icon={
+            <img
+              src="https://lh3.googleusercontent.com/ogw/AGvuzYb8Az9mueXWBCrZlm6FogE9a6D-WAWivPYEKrkB7g=s32-c-mo"
+              className="w-10 rounded-full"
+            />
+          }
+          text="Vivek Maddineni"
+          link="settings"
         />
       </div>
     </div>
-  )
+  );
 }
 
-function Icon(props: {icon: ReactElement, text: string, link: string}){
-  const {icon, text, link} = props;
+function Icon(props: { icon: ReactElement; text: string; link: string }) {
+  const { icon, text, link } = props;
   const navigate = useNavigate();
 
   const isActive = useLocation().pathname.includes(link);
-  
-  return(
+
+  return (
     <div className="center">
       <button
-      onClick={() => {
-        navigate(link);
-      }}
-      className={`outline-none peer w-full aspect-square center rounded-[32px] border-2 transition border-transparent ${isActive? "bg-primary rounded glow-primary" : "hover:rounded hover:border-secondary hover:bg-background-light"}`}>
+        onClick={() => {
+          navigate(link);
+        }}
+        className={`outline-none peer w-full aspect-square center rounded-[32px] border-2 transition border-transparent ${
+          isActive
+            ? "bg-primary rounded glow-primary"
+            : "hover:rounded hover:border-secondary hover:bg-background-light"
+        }`}
+      >
         {icon}
       </button>
-      <Tooltip text={text}/>
+      <Tooltip text={text} />
     </div>
-  )
+  );
 }
 
-function Tooltip(props: {text: string}){
-  return(
+function Tooltip(props: { text: string }) {
+  return (
     <span className="absolute bg-background-light/50 backdrop-blur-sm border-2 border-secondary peer-hover:opacity-100 opacity-0 transition px-2 py-1 left-24 rounded scale-0 peer-hover:scale-100 whitespace-nowrap">
       {props.text}
     </span>
-  )
+  );
 }
 
-function SideSelector(){
+function SideSelector() {
   const queryClient = useQueryClient();
   const dispatch = useAppDispatch();
-  const side = useAppSelector(state => state.app.side);
+  const side = useAppSelector((state) => state.app.side);
 
-  return(
+  return (
     <div className="center">
-      <select className="peer w-full group bg-background hover:bg-background-light border-transparent hover:border-secondary rounded-3xl hover:rounded
+      <select
+        className="peer w-full group bg-background hover:bg-background-light border-transparent hover:border-secondary rounded-3xl hover:rounded
       border-2 h-10 transition outline-none text-center appearance-none"
-      onChange={(e) => {
-        dispatch(setSide(e.target.value as Side));
-        queryClient.refetchQueries({queryKey: "cards", exact: true});
-        queryClient.refetchQueries({queryKey: "team", exact: true});
-      }}
-      value={side}>
+        onChange={(e) => {
+          dispatch(setSide(e.target.value as Side));
+          queryClient.refetchQueries({ queryKey: "cards", exact: true });
+          queryClient.refetchQueries({ queryKey: "team", exact: true });
+        }}
+        value={side}
+      >
         <option value="AFF">AFF</option>
         <option value="NEG">NEG</option>
       </select>
-      <Tooltip text="Side"/>
+      <Tooltip text="Side" />
     </div>
-  )
+  );
 }
 
-function TopicSelector(){
+function TopicSelector() {
   const queryClient = useQueryClient();
   const dispatch = useAppDispatch();
-  const topic = useAppSelector(state => state.app.topic);
+  const topic = useAppSelector((state) => state.app.topic);
 
-  const {isLoading, data: topics} = useQuery('topics', getTopics);
+  const { isLoading, data: topics } = useQuery("topics", getTopics);
 
-  return(
+  return (
     <div className="center">
-      <select className={`peer w-full group bg-background hover:bg-background-light border-transparent hover:border-secondary rounded-3xl hover:rounded
-      border-2 h-10 transition outline-none text-center appearance-none ${isLoading? "border-x-background-light rounded animate-pulse" : ""}`}
-      onChange={(e) => {
-        dispatch(setTopic(e.target.value));
-        queryClient.refetchQueries({queryKey: "cards", exact: true});
-        queryClient.refetchQueries({queryKey: "team", exact: true});
-      }}
-      disabled={isLoading}
-      value={topic}>
+      <select
+        className={`peer w-full group bg-background hover:bg-background-light border-transparent hover:border-secondary rounded-3xl hover:rounded
+      border-2 h-10 transition outline-none text-center appearance-none ${
+        isLoading ? "border-x-background-light rounded animate-pulse" : ""
+      }`}
+        onChange={(e) => {
+          dispatch(setTopic(e.target.value));
+          queryClient.refetchQueries({ queryKey: "cards", exact: true });
+          queryClient.refetchQueries({ queryKey: "team", exact: true });
+        }}
+        disabled={isLoading}
+        value={topic}
+      >
         {topics?.map((t) => (
-          <option key={t} value={t}>{t}</option>
+          <option key={t} value={t}>
+            {t}
+          </option>
         ))}
       </select>
-      <Tooltip text={`Side ${isLoading? "- Loading" : ""}`}/>
+      <Tooltip text={`Topic ${isLoading ? "- Loading" : ""}`} />
     </div>
-  )
+  );
 }
--- src/pages/cards/create/evidence/CreateEvidencePage.tsx
+++ src/pages/cards/create/evidence/CreateEvidencePage.tsx
@@ -7,7 +7,7 @@ import { ExclamationCircle } from "react-bootstrap-icons";
 import { useQueryClient } from "react-query";
 import ContSubSelector from "../../../../components/UI/selectors/ContSubSelector";
 
-export default function CreateEvidencePage(props: {editCard?: Evidence}){
+export default function CreateEvidencePage(props: { editCard?: Evidence }) {
   const navigate = useNavigate();
   const user = useQueryClient().getQueryData("currentUser") as User;
 
@@ -15,131 +15,165 @@ export default function CreateEvidencePage(props: {editCard?: Evidence}){
 
   const [isSaving, setIsSaving] = useState(false);
 
-  const [card, setCard] = useState<Evidence>(props.editCard?? {
-    type: "evidence",
-    cardID: "",
-    ownerUID: user.uid,
-    school: user.school,
-    isPublic: false,
-    createTime: 0,
-    lastEditTime: 0,
-    title: "",
-    sourceName: "",
-    sourceLink: "",
-    data: "",
-    warrant: "",
-    contSub: {
-      contentionID: null,
-      subpointID: null,
+  const [card, setCard] = useState<Evidence>(
+    props.editCard ?? {
+      type: "evidence",
+      cardID: "",
+      ownerUID: user.uid,
+      school: user.school,
+      isPublic: false,
+      createTime: 0,
+      lastEditTime: 0,
+      title: "",
+      sourceName: "",
+      sourceLink: "",
+      data: "",
+      warrant: "",
+      contSub: {
+        contentionID: null,
+        subpointID: null,
+      },
     }
-  });
+  );
 
   useEffect(() => {
     setErrMsg("");
-  }, [card])
+  }, [card]);
 
-  return(
-    <div className="relative w-full h-full flex">
-      <div className={`absolute top-0 bottom-0 left-0 right-0 w-full h-full backdrop-blur-sm z-20 transition opacity-0 flex justify-center items-center ${isSaving? "opacity-100" : "pointer-events-none"}`}>
+  return (
+    <div className="flex relative w-full h-full">
+      <div
+        className={`absolute top-0 bottom-0 left-0 right-0 w-full h-full backdrop-blur-sm z-20 transition opacity-0 flex justify-center items-center ${
+          isSaving ? "opacity-100" : "pointer-events-none"
+        }`}
+      >
         <div className="!backdrop-blur-3xl flex flex-col justify-center items-center animate-pulse">
-          <img src="/DebateToolLogo.svg" className="w-48 aspect-square"/>
+          <img src="/DebateToolLogo.svg" className="w-48 aspect-square" />
           <div className="text-2xl">Saving Card...</div>
         </div>
       </div>
 
-      <div className="w-full md:w-1/2 lg:w-2/3 h-full p-4 flex flex-col space-y-4">
+      <div className="h-screen w-full flex flex-col p-4 space-y-4">
         <div className="flex flex-col space-y-4">
-          <input type="text" 
-          className="input text-text-light" 
-          placeholder="Title"
-          value={card.title}
-          onChange={e => {
-            setCard(old => ({...old, title: e.target.value}))
-          }}
+          <input
+            type="text"
+            className="input text-text-light"
+            placeholder="Title"
+            value={card.title}
+            onChange={(e) => {
+              setCard((old) => ({ ...old, title: e.target.value }));
+            }}
           />
 
           <ContSubSelector
-          default={card.contSub}
-          onChange={contSub => {
-            setCard(old => ({...old, contSub: {contentionID: contSub.contentionID, subpointID: contSub.subpointID}}));
-          }}
+            default={card.contSub}
+            onChange={(contSub) => {
+              setCard((old) => ({
+                ...old,
+                contSub: {
+                  contentionID: contSub.contentionID,
+                  subpointID: contSub.subpointID,
+                },
+              }));
+            }}
           />
 
-          <input 
-          value={card.sourceName}
-          type="text" 
-          className="input !text-lg" 
-          placeholder="Source Name"
-          onChange={e => {
-            setCard(old => ({...old, sourceName: e.target.value}))
-          }}
+          <input
+            value={card.sourceName}
+            type="text"
+            className="input !text-lg"
+            placeholder="Source Name"
+            onChange={(e) => {
+              setCard((old) => ({ ...old, sourceName: e.target.value }));
+            }}
           />
 
-          <input 
-          value={card.sourceLink}
-          type="text" 
-          className="input !text-lg text-evidence" 
-          placeholder="Source Link"
-          onChange={e => {
-            setCard(old => ({...old, sourceLink: e.target.value}))
-          }}
+          <input
+            value={card.sourceLink}
+            type="text"
+            className="input !text-lg text-evidence"
+            placeholder="Source Link"
+            onChange={(e) => {
+              setCard((old) => ({ ...old, sourceLink: e.target.value }));
+            }}
           />
         </div>
 
-        <textarea 
-        value={card.data}
-        className="input !h-full !text-lg resize-none"
-        placeholder="Data (text evidence)"
-        onChange={e => {
-          setCard(old => ({...old, data: e.target.value}))
-        }}
-        />
+        <div className="h-full w-full space-y-4 pb-4">
+          <textarea
+            value={card.data}
+            className="input flex !h-1/2 !text-lg resize-none"
+            placeholder="Data (text evidence)"
+            onChange={(e) => {
+              setCard((old) => ({ ...old, data: e.target.value }));
+            }}
+          />
 
-        <textarea 
-        value={card.warrant}
-        className="input !h-full !text-lg resize-none"
-        placeholder="Warrant (logical reasoning)"
-        onChange={e => {
-          setCard(old => ({...old, warrant: e.target.value}))
-        }}
-        />
+          <textarea
+            value={card.warrant}
+            className="input flex !h-1/2 !text-lg resize-none"
+            placeholder="Warrant (logical reasoning)"
+            onChange={(e) => {
+              setCard((old) => ({ ...old, warrant: e.target.value }));
+            }}
+          />
+        </div>
 
         <div className="w-full flex md:hidden justify-center space-x-4">
           <PublicPrivateSelector
             default={card.isPublic}
-            onChange={e => {
-              setCard(old => ({...old, isPublic: e}))
-          }}
+            onChange={(e) => {
+              setCard((old) => ({ ...old, isPublic: e }));
+            }}
           />
-          <button className="button-evidence !w-32 text-text-light">Save Card</button>
-          </div>
+          <button className="button-evidence !w-32 text-text-light">
+            Save Card
+          </button>
+        </div>
       </div>
-      
-      <div className="relative hidden md:flex flex-col items-center justify-center md:w-1/2 lg:w-1/3 h-full p-4 pl-0">
+
+      <div className="relative opacity-0 md:opacity-100 w-0 md:w-[600px] flex flex-col items-center justify-center h-full p-4 pl-0">
         <h1 className="text-2xl text-evidence">Preview</h1>
         <div className="text-xl text-primary pb-2 flex space-x-2">
-          <ExclamationCircle size={errMsg? 25 : 0} opacity={errMsg? 100 : 0}/>
+          <ExclamationCircle
+            size={errMsg ? 25 : 0}
+            opacity={errMsg ? 100 : 0}
+          />
           <div>{errMsg}</div>
         </div>
-        <div className="h-1/2 w-full"><EvidenceCard card={card}/></div>
+        <div className="h-1/2 w-full">
+          <EvidenceCard card={card} />
+        </div>
         <div className="flex flex-col space-y-4 mt-4">
           <PublicPrivateSelector
             default={card.isPublic}
-            onChange={e => {
-              setCard(old => ({...old, isPublic: e}))
-          }}
+            onChange={(e) => {
+              setCard((old) => ({ ...old, isPublic: e }));
+            }}
           />
-          <button className="button-evidence !w-32 text-text-light" onClick={() => {
-            if(!card.data || !card.title || !card.sourceName || !card.sourceLink || !card.warrant){
-              setErrMsg("Please fill all fields.");
-              return;
-            }
-            setIsSaving(true);
-            saveCard(card)
-            .then(() => {navigate("/cards")})
-          }}>Save Card</button>
+          <button
+            className="button-evidence !w-32 text-text-light"
+            onClick={() => {
+              if (
+                !card.data ||
+                !card.title ||
+                !card.sourceName ||
+                !card.sourceLink ||
+                !card.warrant
+              ) {
+                setErrMsg("Please fill all fields.");
+                return;
+              }
+              setIsSaving(true);
+              saveCard(card).then(() => {
+                navigate("/cards");
+              });
+            }}
+          >
+            Save Card
+          </button>
         </div>
       </div>
     </div>
-  )
+  );
 }
